5da1f19a25c4eba21001836b8394b109120f46cf
Otmane Aatik <aaotmane@gmail.com>
2023-06-20 13:53:13 -0400

    output

--- 01_output.php
+++ 01_output.php
@@ -1,6 +1,16 @@
 <?php
 /* -------- Output & Comments ------- */
+echo 123.2;
+echo "Hello!<br/>";
 
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
+
+// print single values and arrays;
+print_r([1,4,7,9]);
+
+// print more info => data type and length
+var_dump('hello');
+
+// var_export(1234);
