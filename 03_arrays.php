9ac529ecb38a4c7b55e98daa9a803f27b850a42b
Otmane Aatik <aaotmane@gmail.com>
2023-06-20 18:55:04 -0400

    Arraya

--- 03_arrays.php
+++ 03_arrays.php
@@ -4,3 +4,25 @@
 /*
   Arrays hold multiple values. Each value in an array is called an "element"
 */
+
+$numbers = [89, 44,34,67, 99];
+$fruits = ["apple", "banana", "orange"];
+$veggies = array('tomatoes', 'onions');
+
+// echo $veggies;
+// print_r($numbers);
+// var_dump($numbers);
+// echo $fruits[2];
+
+//Associative Array
+$person = [
+  'first_name' => 'Brad',
+  'last_name' => 'Adam'
+];
+
+echo $person['first_name'];
+
+var_dump(json_encode($person));
+//json_encode to get Json format
+
+
