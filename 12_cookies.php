53ba088fbd241da10b78f24570770cdca55a521e
Otmane Aatik <aaotmane@gmail.com>
2023-06-21 13:39:04 -0400

    cookies in PHP

--- 12_cookies.php
+++ 12_cookies.php
@@ -4,3 +4,12 @@
 /*
   Cookies are a mechanism for storing data in the remote browser and thus tracking or identifying return users. You can set specific data to be stored in the browser, and then retrieve it when the user visits the site again.
 */
+//! Set cookie
+
+setcookie('name', 'Otmane', time() + 86400 * 7);
+
+//! Use the cookie
+
+if(isset($_COOKIE['name'])) {
+  echo $_COOKIE['name'];
+}
