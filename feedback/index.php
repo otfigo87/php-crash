7224b6c0a2c579f0ae9d9b418d056c077bc1a119
Otmane Aatik <aaotmane@gmail.com>
2023-06-26 12:32:05 -0400

    Inser Data form mysql

--- feedback/index.php
+++ feedback/index.php
@@ -1,23 +1,75 @@
 <?php include 'inc/header.php' ?>
-    <img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
-    <h2>Feedback</h2>
-    <p class="lead text-center">Leave feedback for Traversy Media</p>
-    <form action="" class="mt-4 w-75">
-      <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
-      </div>
-      <div class="mb-3">
-        <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
-      </div>
-      <div class="mb-3">
-        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
-      </div>
-    </form>
-    <?php include 'inc/footer.php' ?>
 
+<?php
+
+$name = $email = $body = '';
+$nameErr = $emailErr = $bodyErr = '';
+
+//Form submit
+if (isset($_POST['submit'])) {
+  //Validate name
+  if (empty($POST['name'])) {
+    $nameErr = 'Name is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+  //Validate email
+  if (empty($POST['email'])) {
+    $nameErr = 'Email is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  }
+
+  //Validate body
+  if (empty($POST['body'])) {
+    $nameErr = 'Text is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+  //If no error add to database
+  if(empty($nameErr) && empty($emailErr) && empty($bodyErr)) {
+    $sql = "INSERT INTO feedback (name, email, body) VALUES ('$name', '$email', '$body')";
+    if(mysqli_query($conn, $sql)) {
+      //Success
+      header('Location: feedback.php');
+    } else {
+      //Error
+      echo 'Error: ' . mysqli_error($conn);
+    }
+  }
+}
+
+// echo $name;
+// echo $nameErr;
+?>
+<img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
+<h2>Feedback</h2>
+<p class="lead text-center">Leave feedback for Traversy Media</p>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="postPOST" class="mt-4 w-75">
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control <?php echo $nameErr ? 'is-invalid' : null; ?>" id="name" name="name" placeholder="Enter your name">
+    <div class="invalid-feedback">
+      <?php echo $nameErr; ?>
+    </div>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control <?php echo $emailErr ? 'is-invalid' : null; ?>" id="email" name="email" placeholder="Enter your email">
+    <div class="invalid-feedback">
+      <?php echo $emailErr; ?>
+    </div>
+  </div>
+  <div class="mb-3">
+    <label for="body" class="form-label">Feedback</label>
+    <textarea class="form-control <?php echo $bodyErr ? 'is-invalid' : null; ?>" id="body" name="body" placeholder="Enter your feedback"></textarea>
+  </div>
+  <div class="mb-3">
+    <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+  </div>
+</form>
+
+
+<?php include 'inc/footer.php' ?>
